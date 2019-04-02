1f0b09f19a6f3c20da9e3c257cb0be653efe0e3a
rbarker2012 <rbarker2012@gmail.com>
2019-04-02 11:19:58 -0600

    added template for modal component

---
+++ src/components/modals/blog-modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ReactModal from "react-modal";
+
+export default class BlogModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <ReactModal isOpen={this.props.modalIsOpen}>
+        <h1>I am in a modal</h1>
+      </ReactModal>
+    );
+  }
+}
